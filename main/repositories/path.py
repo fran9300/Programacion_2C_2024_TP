c46421c6277252aa60ede62ebf6b461e3cc61676
AgustinaMieres <agustina-mieres@hotmail.com>
2024-10-14 21:25:12 -0300

    planificación de funciones

---
+++ main/repositories/path.py
@@ -0,0 +1,9 @@
+
+#A la hora de usar archivos se debe usar esto para que los paths esten encapsulados. En caso de mover los archivos solo habra que modificar el path aca.
+
+paths = [{
+    "USER":"main/repositories/nombre a remplazar",
+    "MOVIE":"main/repositories/nombre a remplazar",
+    "ROOM":"main/repositories/nombre a remplazar",
+    "PAYMENT_METHODS":"main/repositories/nombre a remplazar",
+}]
